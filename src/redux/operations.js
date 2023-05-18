f2454eccff735d0def2433e1b9107f8180034b75
SerhiiRodin <rodinserj@ukr.net>
2023-05-19 00:21:39 +0300

    add getUsers thunk

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -5,6 +5,23 @@ import 'react-toastify/dist/ReactToastify.css';
 
 axios.defaults.baseURL = 'https://64520ce4a2860c9ed400a177.mockapi.io';
 
+export const getUsersAction = createAsyncThunk(
+  'users/getUsers',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/users');
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      console.log(error.message);
+      toast.error(`Error!!! Something went wrong. Use the service over time.`, {
+        autoClose: 2000,
+      });
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
 export const getContactsAction = createAsyncThunk(
   'contacts/getContacts',
   async (_, thunkAPI) => {
